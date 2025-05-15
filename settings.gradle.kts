cf19cd05adf050e29ad3f338c941f42a9ca89e7a
Nicholas Doglio <3968678+WhosNickDoglio@users.noreply.github.com>
2025-05-15 14:49:00 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2025 Nicholas Doglio
+ * SPDX-License-Identifier: MIT
+ */
+
+rootProject.name = "KotlinProjectTemplate"
+
+pluginManagement {
+    repositories {
+        exclusiveContent {
+            forRepository { google() }
+            filter {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroup("com.google.testing.platform")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        exclusiveContent {
+            forRepository { google() }
+            filter {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroup("com.google.testing.platform")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.develocity") version "4.0.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.2.1"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include(":app")
