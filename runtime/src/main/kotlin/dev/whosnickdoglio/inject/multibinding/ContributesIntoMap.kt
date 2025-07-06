17f13bc55d3f14cef7560eaf22492894f20777c3
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-07-06 16:11:44 -0400

    fix ci

--- runtime/src/main/kotlin/dev/whosnickdoglio/inject/multibinding/ContributesIntoMap.kt
+++ runtime/src/main/kotlin/dev/whosnickdoglio/inject/multibinding/ContributesIntoMap.kt
@@ -20,12 +20,13 @@ public annotation class ContributesIntoMap(
     val boundType: KClass<*> = Unit::class,
     // TODO not sure if I can support this
     // https://github.com/amzn/kotlin-inject-anvil/blob/404411644b27c9f8ecf63c3bba30fc1233b5b489/compiler/src/main/kotlin/software/amazon/lastmile/kotlin/inject/anvil/processor/MergeComponentProcessor.kt#L192
-//    /**
-//     * This contributed binding will replace these contributed classes. The array is allowed to
-//     * include other contributed bindings and contributed component interfaces. All replaced classes
-//     * must use the same scope.
-//     */
-//    val replaces: Array<KClass<*>> = [],
+
+    /**
+     * This contributed binding will replace these contributed classes. The array is allowed to
+     * include other contributed bindings and contributed component interfaces. All replaced classes
+     * must use the same scope.
+     */
+    //    val replaces: Array<KClass<*>> = [],
     /**
      * Whether the qualifier for this class should be included in the generated multibinding method.
      * This parameter is only necessary to use when
