04ac60363b285e31c7d2457341927e311eb8b9f5
nicholasdoglio <NicholasDoglio@Gmail.com>
2025-12-22 13:04:18 -0500

    convention 0.3.0 and detekt 2.x

--- samples/counter/build.gradle.kts
+++ samples/counter/build.gradle.kts
@@ -1,15 +1,9 @@
-import io.gitlab.arturbosch.detekt.Detekt
-
 // Copyright (C) 2025 Nicholas Doglio
 // SPDX-License-Identifier: MIT
 plugins {
     alias(libs.plugins.convention.jvm)
     alias(libs.plugins.ksp)
     // TODO multiplatform
-}
-
-tasks.withType(Detekt::class.java).configureEach {
-    exclude { fileTreeElement -> fileTreeElement.file.path.contains("build/generated") }
 }
 
 dependencies {
