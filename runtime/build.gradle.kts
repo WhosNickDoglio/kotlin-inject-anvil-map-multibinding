ec24eb0ae2526aa6fa6bef71474314d50720d561
nicholasdoglio <NicholasDoglio@Gmail.com>
2026-05-15 23:38:19 -0400

    Add KMP targets

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -2,10 +2,43 @@
 // SPDX-License-Identifier: MIT
 plugins {
     alias(libs.plugins.convention.kmp)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.dokka)
     alias(libs.plugins.publish)
 }
 
-kotlin { jvm() }
+kotlin {
+    android {
+        namespace = "dev.whosnickdoglio.inject.multibinding"
+        compileSdk { version = release(37) }
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
+    js { browser() }
+
+    jvm()
+
+    linuxArm64()
+    linuxX64()
+
+    macosArm64()
+
+    mingwX64()
+
+    tvosArm64()
+    tvosSimulatorArm64()
+
+    // https://youtrack.jetbrains.com/issue/KT-84582/coreLibrariesVersion-isnt-really-compatible-with-JS-or-Wasm
+    //    wasmJs { browser() }
+
+    watchosArm32()
+    watchosArm64()
+    watchosSimulatorArm64()
+
+    applyDefaultHierarchyTemplate()
+}
 
 dependencies { commonMainImplementation(libs.kotlin.inject.anvil.runtime) }
